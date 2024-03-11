36808679d9d87d29cfac8c9345faafa5ebf09d57
GlebAlekseev <glebalekseevjk@gmail.com>
2024-03-11 23:27:04 +0300

    feat: implement methods in BookItemRepository

--- feature/book/api/src/main/kotlin/pro/linguistcopilot/feature/book/repository/BookItemRepository.kt
+++ feature/book/api/src/main/kotlin/pro/linguistcopilot/feature/book/repository/BookItemRepository.kt
@@ -6,4 +6,9 @@ import pro.linguistcopilot.feature.book.entity.BookItem
 interface BookItemRepository {
     val bookItems: Flow<List<BookItem>>
     fun addBookItem(bookItem: BookItem)
+    fun addTemporaryBookItem(bookItem: BookItem): BookItem
+    fun removeTemporaryBookItemById(id: String)
+    fun updateTemporaryBookItem(bookItem: BookItem)
+    suspend fun isExist(hash: String): Boolean
+    fun removeTemporaryBookItemAndAddBookItem(bookItem: BookItem)
 }
--- feature/book/impl/src/main/kotlin/pro/linguistcopilot/feature/book/BookItemRepositoryImpl.kt
+++ feature/book/impl/src/main/kotlin/pro/linguistcopilot/feature/book/BookItemRepositoryImpl.kt
@@ -2,41 +2,81 @@ package pro.linguistcopilot.feature.book
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.first
 import pro.linguistcopilot.di.scope.AppComponentScope
 import pro.linguistcopilot.feature.book.entity.BookItem
 import pro.linguistcopilot.feature.book.repository.BookItemRepository
 import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.UUID
 import javax.inject.Inject
+import kotlin.random.Random
 
-val sampleList = listOf(
-    BookItem("Book 1", SimpleDateFormat("dd.MM.yyyy").parse("29.12.2023")),
-    BookItem("Book 2", SimpleDateFormat("dd.MM.yyyy").parse("29.12.2023")),
-    BookItem("Book 3", SimpleDateFormat("dd.MM.yyyy").parse("29.12.2023")),
-    BookItem("Book 4", SimpleDateFormat("dd.MM.yyyy").parse("29.12.2023")),
-    BookItem("Book 5", SimpleDateFormat("dd.MM.yyyy").parse("29.12.2023")),
-    BookItem("Book 6", SimpleDateFormat("dd.MM.yyyy").parse("30.12.2023")),
-    BookItem("Book 7", SimpleDateFormat("dd.MM.yyyy").parse("30.12.2023")),
-    BookItem("Book 8", SimpleDateFormat("dd.MM.yyyy").parse("30.12.2023")),
-    BookItem("Book 9", SimpleDateFormat("dd.MM.yyyy").parse("30.12.2023")),
-    BookItem("Book 10", SimpleDateFormat("dd.MM.yyyy").parse("30.12.2023")),
-    BookItem("Book 11", SimpleDateFormat("dd.MM.yyyy").parse("31.12.2023")),
-    BookItem("Book 12", SimpleDateFormat("dd.MM.yyyy").parse("31.12.2023")),
-    BookItem("Book 13", SimpleDateFormat("dd.MM.yyyy").parse("31.12.2023")),
-    BookItem("Book 14", SimpleDateFormat("dd.MM.yyyy").parse("31.12.2023")),
-    BookItem("Book 15", SimpleDateFormat("dd.MM.yyyy").parse("31.12.2023")),
-    BookItem("Book 16", SimpleDateFormat("dd.MM.yyyy").parse("31.12.2023")),
-    BookItem("Book 17", SimpleDateFormat("dd.MM.yyyy").parse("31.12.2023")),
-    BookItem("Book 18", SimpleDateFormat("dd.MM.yyyy").parse("31.12.2023")),
-    BookItem("Book 19", SimpleDateFormat("dd.MM.yyyy").parse("31.12.2023")),
-    BookItem("Book 20", SimpleDateFormat("dd.MM.yyyy").parse("31.12.2023"))
-)
+val sampleList: List<BookItem> by lazy {
+    val dateFormatter = SimpleDateFormat("yyyy-MM-dd")
+    val startDate = dateFormatter.parse("2023-11-23")!!
+    val endDate = dateFormatter.parse("2023-11-27")!!
+
+    (1..10).map {
+        val title = "Book $it"
+        val uri = UUID.randomUUID().toString()
+        val createdAt = generateRandomDate(startDate, endDate)
+        val hash = UUID.randomUUID().toString()
+
+        BookItem(title = title, uri = uri, createdAt = createdAt, hash = hash)
+    }
+}
+
+private fun generateRandomDate(startDate: Date, endDate: Date): Date {
+    val random = Random.nextLong(startDate.time, endDate.time)
+    return Date(random)
+}
 
 
 @AppComponentScope
 class BookItemRepositoryImpl @Inject constructor() : BookItemRepository {
-    private val bookList = MutableStateFlow(sampleList)
-    override val bookItems: Flow<List<BookItem>> = bookList
+    private val temporaryBookList =
+        MutableStateFlow(sampleList)
+    private val bookList = MutableStateFlow(listOf<BookItem>())
+    override val bookItems: Flow<List<BookItem>>
+        get() = combine(bookList, temporaryBookList) { dbList, tempList ->
+            (dbList + tempList).sortedByDescending { it.createdAt }
+        }
+
     override fun addBookItem(bookItem: BookItem) {
-        bookList.tryEmit(bookList.value + bookItem)
+        bookList.value += bookItem
+    }
+
+    override fun addTemporaryBookItem(bookItem: BookItem): BookItem {
+        temporaryBookList.value += bookItem
+        return bookItem
+    }
+
+    override fun removeTemporaryBookItemById(id: String) {
+        val newList = temporaryBookList.value.toMutableList().apply {
+            removeIf { it.id == id }
+        }
+        temporaryBookList.tryEmit(newList)
+    }
+
+    override fun updateTemporaryBookItem(bookItem: BookItem) {
+        val newList = temporaryBookList.value.toMutableList().apply {
+            removeIf { it.id == bookItem.id }
+            add(bookItem)
+        }
+        temporaryBookList.tryEmit(newList)
+    }
+
+    override suspend fun isExist(hash: String): Boolean {
+        return bookItems.first().find { it.hash == hash } != null
+    }
+
+    override fun removeTemporaryBookItemAndAddBookItem(bookItem: BookItem) {
+        val newList = temporaryBookList.value.toMutableList().apply {
+            removeIf { it.id == bookItem.id }
+        }
+        temporaryBookList.tryEmit(newList)
+        bookList.value += bookItem
     }
 }
